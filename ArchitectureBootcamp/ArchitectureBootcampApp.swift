ddad0297c99a80a1eeb10b5f03aafd50752c858a
adammgerber <agerber7777@gmail.com>
2026-04-25 13:46:27 +0300

    Initial Commit

---
+++ ArchitectureBootcamp/ArchitectureBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureBootcampApp.swift
+//  ArchitectureBootcamp
+//
+//  Created by Adam Gerber on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
