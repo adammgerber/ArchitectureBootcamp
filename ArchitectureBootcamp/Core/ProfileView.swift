daa56502ba8c680da882e326c2d3039ffc69c24b
adammgerber <agerber7777@gmail.com>
2026-04-28 11:04:30 +0300

    [Feature] Support child RouterViews

--- ArchitectureBootcamp/Core/ProfileView.swift
+++ ArchitectureBootcamp/Core/ProfileView.swift
@@ -38,49 +38,161 @@ extension View {
 }
 
 protocol Router {
-    func showScreen<T: View>(@ViewBuilder destination: () -> T)
+    func showScreen<T: View>(@ViewBuilder destination: @escaping (Router) -> T)
+    func dismissScreen()
 }
 
 struct RouterView<Content: View>: View, Router {
     
+    @Environment(\.dismiss) private var dismiss
+
     @State private var path: [AnyDestination] = []
+    
+    // Binding to the view stack from previous RouterViews
+    @Binding var screenStack: [AnyDestination]
+    
+    var addNavigationView: Bool
     @ViewBuilder var content: (Router) -> Content
+    
+    init(
+        screenStack: (Binding<[AnyDestination]>)? = nil,
+        addNavigationView: Bool = true,
+        content: @escaping (Router) -> Content
+    ) {
+        self._screenStack = screenStack ?? .constant([])
+        self.addNavigationView = addNavigationView
+        self.content = content
+    }
 
     var body: some View {
-        NavigationStack(path: $path) {
+        NavigationStackIfNeeded(path: $path, addNavigationView: addNavigationView) {
             content(self)
-                .navigationDestination(for: AnyDestination.self) { value in
-                    value.destination
-                }
         }
     }
     
-    func showScreen<T: View>(@ViewBuilder destination: () -> T) {
-        let destination = AnyDestination(destination: destination())
-        path.append(destination)
+    func showScreen<T: View>(@ViewBuilder destination: @escaping (Router) -> T) {
+        let screen = RouterView<T>(
+            screenStack: screenStack.isEmpty ? $path : $screenStack,
+            addNavigationView: false
+        ) { newRouter in
+            destination(newRouter)
+        }
+        
+        let destination = AnyDestination(destination: screen)
+        
+        if screenStack.isEmpty {
+            // This means we are in the first RouterView
+            path.append(destination)
+        } else {
+            // This means we are in a secondary RouterView
+            screenStack.append(destination)
+        }
+    }
+    
+    func dismissScreen() {
+        dismiss()
     }
 }
 
-struct ProfileView: View {
+/*
+ RouterView - @Environment
+    ProfileView
+        RouterView - @Environment
+            SettingsView
+                RouterView - @Environment
+                    AccountView
+ */
+
+struct NavigationStackIfNeeded<Content: View>: View {
     
-    @State private var path: [AnyDestination] = []
+    @Binding var path: [AnyDestination]
+    var addNavigationView: Bool = true
+    @ViewBuilder var content: Content
     
     var body: some View {
-        RouterView { router in
-            VStack(spacing: 40) {
-                Button {
-                    router.showScreen {
-                        Color.blue
-                            .toolbarVisibility(.hidden, for: .navigationBar)
+        if addNavigationView {
+            NavigationStack(path: $path) {
+                content
+                    .navigationDestination(for: AnyDestination.self) { value in
+                        value.destination
                     }
-                } label: {
-                    Text("Click me")
+            }
+        } else {
+            content
+        }
+    }
+}
+
+
+struct ProfileView: View {
+            
+    let router: Router
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            Button {
+                router.showScreen { router in
+                    SettingsView(router: router)
                 }
+            } label: {
+                Text("Click me")
             }
         }
     }
 }
 
+struct SettingsView: View {
+    
+    let router: Router
+    
+    var body: some View {
+        VStack {
+            Text("Settings")
+            
+            Button {
+                router.showScreen { router in
+                    AccountView(router: router)
+                }
+            } label: {
+                Text("Go forward")
+            }
+            Button {
+                router.dismissScreen()
+            } label: {
+                Text("Dismiss")
+            }
+        }
+        .navigationTitle("Settings")
+    }
+}
+
+struct AccountView: View {
+    
+    let router: Router
+    
+    var body: some View {
+        VStack {
+            Text("Account")
+            
+            Button {
+                router.showScreen { router in
+                    AccountView(router: router)
+                }
+            } label: {
+                Text("Go forward")
+            }
+            Button {
+                router.dismissScreen()
+            } label: {
+                Text("Dismiss screen")
+            }
+        }
+        .navigationTitle("Account")
+    }
+}
+
 #Preview {
-    ProfileView()
+    RouterView { router in
+        ProfileView(router: router)
+    }
 }
